193887bfadf23a07efc31f4b9e3f6c7258ddcf6f
Phat <143582741+phatnng@users.noreply.github.com>
2025-11-02 18:12:10 -0800

    commit

--- 05_Day_Arrays/05_day_starter/scripts/main.js
+++ 05_Day_Arrays/05_day_starter/scripts/main.js
@@ -1,3 +1,75 @@
-console.log(countries)
-alert('Open the browser console whenever you work on JavaScript')
-alert('Open the console and check if the countries has been loaded')
+// console.log(countries)
+// alert('Open the browser console whenever you work on JavaScript')
+// alert('Open the console and check if the countries has been loaded')
+
+
+let arr = [5, 4 , 3, "One", "Two"]
+let numbers = [10, 20, 30, 40, 50, 60, 70];
+
+
+console.log(arr)
+
+console.log(numbers.length)
+
+console.log(arr[0], arr[2], arr[4])
+
+let mixedDataTypes = [25, "ChatGPT", true, null, { language: "JS" }, [1, 2, 3]];
+console.log("Length of mixedDataTypes:", mixedDataTypes.length);
+
+let itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+console.log(itCompanies)
+console.log(itCompanies.length)
+
+
+let first = itCompanies[0]
+let second = itCompanies[Math.floor(itCompanies.length / 2)]
+let third = itCompanies[itCompanies.length-1]
+
+console.log(first)
+console.log(second)
+console.log(third)
+
+for ( let company of itCompanies){
+    console.log(company)
+}
+
+for(let company of itCompanies){
+    console.log(company.toUpperCase())
+}
+
+console.log(itCompanies.join(", ") + "are big IT companies")
+
+let companyToCheck = "Google";
+if (itCompanies.includes(companyToCheck)) {
+  console.log(`${companyToCheck} exists in the array.`);
+} else {
+  console.log("Company not found.");
+}
+
+
+let companiesWithOneO = [];
+for (let company of itCompanies){
+    let count = 0;
+  for (let char of company.toLowerCase()) {
+    if (char === 'o') count++;
+  }
+  if (count <= 1) companiesWithOneO.push(company);
+}
+console.log("Companies with one or no 'o':", companiesWithOneO);
+
+console.log(itCompanies.sort())
+
+console.log(itCompanies.reverse())
+
+console.log(itCompanies.splice(0,3))
+console.log(itCompanies.splice(-3))
+
+let midIndex = Math.floor(itCompanies.length / 2);
+if (itCompanies.length % 2 === 0) {
+  console.log("Middle companies:", itCompanies.slice(midIndex - 1, midIndex + 1));
+} else {
+  console.log("Middle company:", itCompanies[midIndex]);
+}
+
+console.log("After removing first company:", itCompanies);
+
--- 06_Day_Loops/06_day_starter/scripts/main.js
+++ 06_Day_Loops/06_day_starter/scripts/main.js
@@ -1,2 +1,136 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// 
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+]
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+]
+
+const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+
+// for (let i = 0; i <= countries.length - 1; i++){
+//     console.log(countries[i].join(", "));
+// }
+
+// let i = countries.length
+// while(i){
+//     console.log(countries[i])
+//     i--;
+// }
+
+
+let pattern = ""
+for(let i = 1; i < 7; i++){
+    pattern += "#"
+    console.log(pattern)
+}
+
+// for (let i = 1; i < 11; i++){
+//     let a = i * i;
+//     console.log(i + " x " + i + " = " + a)
+// }
+
+// console.log("i\t i^2\t i^3"); // Header
+
+// for(let i=0; i<11;i++){
+//     console.log(`${i}\t ${i**2} \t ${i**3}`)
+// }
+
+// for(let i=0; i < 101; i++){
+//     if (i%2 == 0){
+//         console.log(i)
+//     }
+// }
+
+// for(let i=0; i<101; i++){
+//     if (i%2 !== 0){
+//         console.log(i)
+//     }
+// }
+
+// let sum = 0;
+
+// for (let i=0; i<=100; i++){
+//     sum += i;
+// }
+
+// console.log(sum)
+
+
+`Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+
+The sum of all evens from 0 to 100 is 2550. And the sum of all odds from 0 to 100 is 2500.`
+
+let evenSum = 0;
+let addSum = 0;
+// for (let i=0; i<=100; i++){
+//     if (i % 2 == 0){
+//         evenSum += i
+//     }
+//     else if(i % 2 !== 0){
+//         addSum += i
+//     }
+// }
+
+// console.log(`The sum of all evens from 0 to 100 is ${evenSum}. And the sum of all adds from 0 to 100 is ${addSum}`)
+
+
+// const tArr = [];
+// for (let i=0; i<=100; i++){
+//     if (i % 2 == 0){
+//         evenSum += i
+//     }
+//     else if(i % 2 !== 0){
+//         addSum += i
+//     }
+// }
+// tArr.push(evenSum)
+// tArr.push(addSum)
+
+// console.log(tArr)
+
+
+// const randomArray = []
+// for (let i = 0; i < 6; i++){
+//     randomArray.push(Math.floor(Math.random() * 100))
+// }
+
+// console.log(randomArray)
+
+// const uniqueArray = []
+// while (uniqueArray.length < 5) {
+//   let num = Math.floor(Math.random() * 100);
+//   if (!uniqueArray.includes(num)) {
+//     uniqueArray.push(num);
+//   }
+// }
+
+
+// console.log(uniqueArray)
+
+let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+let id = '';
+
+for(let i=0; i<6; i++){
+  let randomIndex = Math.floor(Math.random() * chars.length)
+  id +=(chars[randomIndex])
+}
+
+console.log(id)
